fb2b85ecbf7dfcac0477776dd442dc07c072c969
VictorLCM1227 <victor.luan.costa.matos@gmail.com>
2026-09-14 23:55:52 -0300

    logica funcionando

--- main.js
+++ main.js
@@ -6,14 +6,17 @@ const elemento = document.getElementById('num')
 const jogar = document.getElementById('jogar')
 const displayNum = document.getElementById('displayNum')
 const escolhaNum = document.getElementById('escolhaNum')
-
+const resultado = document.getElementById('resultado')
+const repetir = document.getElementById('repetir')
 
 btnImpar.addEventListener('click', function() {
     showOpc('ÍMPAR')
+    opcEscolhida = 'ÍMPAR'
 })
 
 btnPar.addEventListener('click', function() {
     showOpc('PAR')
+    opcEscolhida = 'PAR'
 })
 
 
@@ -36,8 +39,11 @@ window.addEventListener('DOMContentLoaded', function() {
     esconder(elemento)
     esconder(jogar)
     esconder(escolhaNum)
+    esconder(resultado)
+    esconder(repetir)
 })
 
+const opc = document.querySelectorAll(".opc")
 
 const botoes = document.querySelectorAll('.btn-num')
 
@@ -48,3 +54,37 @@ botoes.forEach(botao => {
         mostrar(jogar)
     })
 })
+
+// jogar
+
+jogar.addEventListener('click', processar)
+
+function processar() {
+  let vencedor = ''
+  let impar = ''
+  let par = ''
+  let pc = Math.random()
+  soma = Number(numeroEscolhido) + pc
+  if (soma % 2 == 0) {
+    par = true
+  } else {
+    impar = true
+  }
+
+  if (opcEscolhida == 'PAR') {
+    if (par == true) {
+      vencedor = 'Você'
+    } else {
+      vencedor = 'Pc'
+    }
+  } else {
+    if (impar == true) {
+      vencedor = 'Você'
+    } else {
+      vencedor = 'Pc'
+    }
+  }
+  mostrar(resultado)
+  resultado.innerText = `Vencedor: ${vencedor}`
+  mostrar(repetir)
+}
