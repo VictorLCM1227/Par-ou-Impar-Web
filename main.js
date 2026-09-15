ce0ddd3b12abb06636351f3fcf0b9beb3f35876f
VictorLCM1227 <victor.luan.costa.matos@gmail.com>
2026-09-14 23:29:21 -0300

    começo do jogo

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 var displayOpc = document.getElementById('displayOpc')
 var btnPar = document.getElementById('btn-par')
 var btnImpar = document.getElementById('btn-impar')
+
 const elemento = document.getElementById('num')
+const jogar = document.getElementById('jogar')
+const displayNum = document.getElementById('displayNum')
+const escolhaNum = document.getElementById('escolhaNum')
+
 
 btnImpar.addEventListener('click', function() {
     showOpc('ÍMPAR')
@@ -11,14 +16,35 @@ btnPar.addEventListener('click', function() {
     showOpc('PAR')
 })
 
+
 function showOpc(opc) {
     displayOpc.innerText = `Você escolheu ${opc}`
+    mostrar(elemento)
+    mostrar(escolhaNum)
 }
 
-function esconder() {
-  elemento.style.display = 'none';
+function esconder(item) {
+    item.style.display = 'none'
 }
 
-function mostrar() {
-  elemento.style.display = 'block'; // ou 'flex', 'grid', dependendo do seu layout
+function mostrar(item) {
+    item.style.display = 'block'
 }
+
+
+window.addEventListener('DOMContentLoaded', function() {
+    esconder(elemento)
+    esconder(jogar)
+    esconder(escolhaNum)
+})
+
+
+const botoes = document.querySelectorAll('.btn-num')
+
+botoes.forEach(botao => {
+    botao.addEventListener('click', (evento) => {
+        numeroEscolhido = evento.target.value
+        displayNum.innerHTML = `Você escolheu o número: ${numeroEscolhido}`
+        mostrar(jogar)
+    })
+})
